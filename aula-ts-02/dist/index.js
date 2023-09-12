23a90a996a033500daa670ebc8cd1e527a5efbb6
Wilker Mizael <wilkermizael@Wilker-Macbook-Pro.local>
2023-09-12 15:56:23 -0300

    feito aula 2

---
+++ aula-ts-02/dist/index.js
@@ -0,0 +1,5 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+var result = sum(1, 2);
+console.log(result);
---
+++ aula-ts-02/src/index.ts
@@ -0,0 +1,6 @@
+function sum(num1: number, num2: number){
+    return num1+num2;
+}
+
+const result = sum(1,2);
+console.log(result);
